48f18009df2f9e72dd8123e5b9f96a573c3d1e32
Menezo <joaovitormenezesm12@gmail.com>
2023-11-23 17:50:38 -0300

    Main View

---
+++ src/main/java/com/menezo/workshopjavafxjdbc/Main.java
@@ -0,0 +1,25 @@
+package com.menezo.workshopjavafxjdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        Parent parent = loader.load();
+        Scene mainScene = new Scene(parent);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Sample JavaFX application");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
